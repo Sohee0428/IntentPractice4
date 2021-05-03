19e58a1ba0278c3b1bc72684b0a7ba9bee4cf26a
Sohee <sohee3480@gmail.com>
2021-05-03 19:45:08 +0900

    닉네임 변경을 위한 화면 이동 (startActivityForResult)활용

--- app/src/main/java/com/example/intentpractice4/MainActivity.kt
+++ app/src/main/java/com/example/intentpractice4/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.intentpractice4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_FOR_NICKNAME = 1004
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(this, EditNicknameActivity::class.java)
+            startActivityForResult(myIntent, REQUEST_FOR_NICKNAME)
+        }
     }
 }
